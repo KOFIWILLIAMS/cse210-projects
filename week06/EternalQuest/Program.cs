a743ac38f070486a7ab40d103f7759b8bde5d1a8
KOFIWILLIAMS <winnerwilliams1@gmail.com>
2025-06-16 16:28:19 +0000

    WEEK 6

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        Console.WriteLine("Helloo! Please Welcome to 'Eternal Quest Program' !");
+        Console.WriteLine("Track your goals and level up each day");
+        Console.WriteLine("***************************");
+
+        GoalManager manager = new GoalManager();
+        manager.Start();
+
+        Console.WriteLine("Goodbye, see you next time");
     }
 }
